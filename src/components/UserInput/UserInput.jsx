b16aa1b9781011bb33f0e9ee3bc341407c3efa33
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-01-28 20:23:29 +0200

    feat: Store the values entered by user

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,26 +1,71 @@
+// import { useState } from "react";
 // import "./UserInput.css";
 
 // function UserInput() {
+//   const [userInput, setUserInput] = useState({
+//     initialInvestment: 10000,
+//     annualInvestment: 1200,
+//     expectedReturn: 6,
+//     duration: 10,
+//   });
+
+//   function handleChange(inputIdentifier, newValue) {
+//     setUserInput((prevUserInput) => {
+//       return {
+//         ...prevUserInput,
+//         [inputIdentifier]: newValue,
+//       };
+//     });
+//   }
+
 //   return (
 //     <section className="user-input">
-//       <div className="input-group">
+//       <div className="user-input__group">
 //         <p>
 //           <label className="user-input__label">Initial Investment</label>
-//           <input className="user-input__input" type="number" required />
+//           <input
+//             className="user-input__input"
+//             type="number"
+//             required
+//             value={userInput.initialInvestment}
+//             onChange={(evt) =>
+//               handleChange("initialInvestment", evt.target.value)
+//             }
+//           />
 //         </p>
 //         <p>
 //           <label className="user-input__label">Annual Investment</label>
-//           <input className="user-input__input" type="number" required />
+//           <input
+//             className="user-input__input"
+//             type="number"
+//             required
+//             value={userInput.annualInvestment}
+//             onChange={(evt) =>
+//               handleChange("annualInvestment", evt.target.value)
+//             }
+//           />
 //         </p>
 //       </div>
-//       <div className="input-group">
+//       <div className="user-input__group">
 //         <p>
 //           <label className="user-input__label">Expected Return</label>
-//           <input className="user-input__input" type="number" required />
+//           <input
+//             className="user-input__input"
+//             type="number"
+//             required
+//             value={userInput.expectedReturn}
+//             onChange={(evt) => handleChange("expectedReturn", evt.target.value)}
+//           />
 //         </p>
 //         <p>
 //           <label className="user-input__label">Duration</label>
-//           <input className="user-input__input" type="number" required />
+//           <input
+//             className="user-input__input"
+//             type="number"
+//             required
+//             value={userInput.duration}
+//             onChange={(evt) => handleChange("duration", evt.target.value)}
+//           />
 //         </p>
 //       </div>
 //     </section>
@@ -29,27 +74,74 @@
 
 // export default UserInput;
 
+import { useState } from "react";
 import "./UserInput.css";
 
-function InputField({ label, type }) {
+function InputField({ label, value, onChange }) {
   return (
     <p>
       <label className="user-input__label">{label}</label>
-      <input className="user-input__input" type={type} required />
+      <input
+        className="user-input__input"
+        type="number"
+        required
+        value={value}
+        onChange={onChange}
+      />
     </p>
   );
 }
 
 function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleInputChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section className="user-input">
       <div className="user-input__group">
-        <InputField label="Initial Investment" type="number" />
-        <InputField label="Annual Investment" type="number" />
+        <InputField
+          label="Initial Investment"
+          value={userInput.initialInvestment}
+          onChange={({ target: { value } }) =>
+            handleInputChange("initialInvestment", value)
+          }
+        />
+        <InputField
+          label="Annual Investment"
+          value={userInput.annualInvestment}
+          onChange={({ target: { value } }) =>
+            handleInputChange("annualInvestment", value)
+          }
+        />
       </div>
       <div className="user-input__group">
-        <InputField label="Expected Return" type="number" />
-        <InputField label="Duration" type="number" />
+        <InputField
+          label="Expected Return"
+          value={userInput.expectedReturn}
+          onChange={({ target: { value } }) =>
+            handleInputChange("expectedReturn", value)
+          }
+        />
+        <InputField
+          label="Duration"
+          value={userInput.duration}
+          onChange={({ target: { value } }) =>
+            handleInputChange("duration", value)
+          }
+        />
       </div>
     </section>
   );
