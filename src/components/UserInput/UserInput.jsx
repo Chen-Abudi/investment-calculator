31a4ba2df8fae161268e9416a8ee3337850134ca
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-01-29 20:19:28 +0200

    chore: Clear comments

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,80 +1,3 @@
-// import { useState } from "react";
-// import "./UserInput.css";
-
-// function UserInput() {
-//   const [userInput, setUserInput] = useState({
-//     initialInvestment: 10000,
-//     annualInvestment: 1200,
-//     expectedReturn: 6,
-//     duration: 10,
-//   });
-
-//   function handleChange(inputIdentifier, newValue) {
-//     setUserInput((prevUserInput) => {
-//       return {
-//         ...prevUserInput,
-//         [inputIdentifier]: newValue,
-//       };
-//     });
-//   }
-
-//   return (
-//     <section className="user-input">
-//       <div className="user-input__group">
-//         <p>
-//           <label className="user-input__label">Initial Investment</label>
-//           <input
-//             className="user-input__input"
-//             type="number"
-//             required
-//             value={userInput.initialInvestment}
-//             onChange={(evt) =>
-//               handleChange("initialInvestment", evt.target.value)
-//             }
-//           />
-//         </p>
-//         <p>
-//           <label className="user-input__label">Annual Investment</label>
-//           <input
-//             className="user-input__input"
-//             type="number"
-//             required
-//             value={userInput.annualInvestment}
-//             onChange={(evt) =>
-//               handleChange("annualInvestment", evt.target.value)
-//             }
-//           />
-//         </p>
-//       </div>
-//       <div className="user-input__group">
-//         <p>
-//           <label className="user-input__label">Expected Return</label>
-//           <input
-//             className="user-input__input"
-//             type="number"
-//             required
-//             value={userInput.expectedReturn}
-//             onChange={(evt) => handleChange("expectedReturn", evt.target.value)}
-//           />
-//         </p>
-//         <p>
-//           <label className="user-input__label">Duration</label>
-//           <input
-//             className="user-input__input"
-//             type="number"
-//             required
-//             value={userInput.duration}
-//             onChange={(evt) => handleChange("duration", evt.target.value)}
-//           />
-//         </p>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default UserInput;
-
-// import { useState } from "react";
 import "./UserInput.css";
 
 function InputField({ label, value, onChange }) {
@@ -93,22 +16,6 @@ function InputField({ label, value, onChange }) {
 }
 
 function UserInput({ onChange, userInput }) {
-  // const [userInput, setUserInput] = useState({
-  //   initialInvestment: 10000,
-  //   annualInvestment: 1200,
-  //   expectedReturn: 6,
-  //   duration: 10,
-  // });
-
-  // function handleInputChange(inputIdentifier, newValue) {
-  //   setUserInput((prevUserInput) => {
-  //     return {
-  //       ...prevUserInput,
-  //       [inputIdentifier]: newValue,
-  //     };
-  //   });
-  // }
-
   return (
     <section className="user-input">
       <div className="user-input__group">
